07f9d01eaa3de7de6f6538c8c6bce2b3e2f8fe2b
Kenneth Heimer <37943539+kehe17@users.noreply.github.com>
2018-11-20 17:58:39 -0500

    Updated default style

--- config/page.php
+++ config/page.php
@@ -19,8 +19,9 @@ return [
             "lang" => "sv",
             "stylesheets" => [
                 // Change here to set another default stylesheet
-                //"css/dbwebb-se.min.css",
-                "css/kmom01.css",
+                // "css/dbwebb-se.min.css",
+                // "css/kmom01.css",
+                "css/kmom02.min.css",
             ],
             "javascripts" => [
                 "js/responsive-menu.js",
